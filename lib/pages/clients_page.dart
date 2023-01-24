f3ba596399cbbf99eff92405e3677599dee392b3
julia.cavalcante <ccardozojulia@gmail.com>
2023-01-24 16:49:47 -0300

    bringing informations with provider

--- lib/pages/clients_page.dart
+++ lib/pages/clients_page.dart
@@ -1,6 +1,8 @@
 import 'package:client_control/models/client_type.dart';
 import 'package:client_control/models/client.dart';
+import 'package:client_control/models/clients.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../components/hamburger_menu.dart';
 
@@ -23,34 +25,42 @@ class _ClientsPageState extends State<ClientsPage> {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
       drawer: const HamburgerMenu(),
-      body: ListView.builder(
-        itemCount: clients.length,
-        itemBuilder: (context, index) {
-          return Dismissible(
-            key: UniqueKey(),
-            background: Container(color: Colors.red),
-            child: ListTile(
-              leading: Icon(clients[index].type.icon),
-              title: Text(clients[index].name + ' ('+ clients[index].type.name + ')'),
-              iconColor: Colors.indigo,
-            ),
-            onDismissed: (direction) {
-              setState(() {
-                clients.removeAt(index);
-              });
-            },
-          );
-        },
+      body: Consumer<Clients>(
+          builder: (BuildContext context, Clients list, Widget? widget) {
+            ListView.builder(
+              itemCount: list.clients.length,
+              itemBuilder: (context, index) {
+                return Dismissible(
+                  key: UniqueKey(),
+                  background: Container(color: Colors.red),
+                  child: ListTile(
+                    leading: Icon(list.clients[index].type.icon),
+                    title: Text(list.clients[index].name + ' (' +
+                        list.clients[index].type.name + ')'),
+                    iconColor: Colors.indigo,
+                  ),
+                  onDismissed: (direction) {
+                    setState(() {
+                      list.clients.removeAt(index);
+                    });
+                  },
+                );
+              },
+            );
+          }
+
+
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.indigo,
-        onPressed: (){createType(context);},
+        onPressed: () {
+          createType(context);
+        },
         tooltip: 'Add Tipo',
         child: const Icon(Icons.add),
       ),
@@ -65,32 +75,33 @@ class _ClientsPageState extends State<ClientsPage> {
     showDialog(
         context: context,
         builder: (BuildContext context) {
-            return AlertDialog(
-                scrollable: true,
-                title: const Text('Cadastrar cliente'),
-                content: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Form(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        TextFormField(
-                          controller: nomeInput,
-                          decoration: const InputDecoration(
-                            labelText: 'Nome',
-                            icon: Icon(Icons.account_box),
-                          ),
-                        ),
-                        const Padding(padding: EdgeInsets.all(5)),
-                        TextFormField(
-                          controller: emailInput,
-                          decoration: const InputDecoration(
-                            labelText: 'Email',
-                            icon: Icon(Icons.email),
-                          ),
-                        ),
-                        const Padding(padding: EdgeInsets.all(5)),
-                        StatefulBuilder(builder: (BuildContext context, StateSetter setState) {
+          return AlertDialog(
+            scrollable: true,
+            title: const Text('Cadastrar cliente'),
+            content: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Form(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    TextFormField(
+                      controller: nomeInput,
+                      decoration: const InputDecoration(
+                        labelText: 'Nome',
+                        icon: Icon(Icons.account_box),
+                      ),
+                    ),
+                    const Padding(padding: EdgeInsets.all(5)),
+                    TextFormField(
+                      controller: emailInput,
+                      decoration: const InputDecoration(
+                        labelText: 'Email',
+                        icon: Icon(Icons.email),
+                      ),
+                    ),
+                    const Padding(padding: EdgeInsets.all(5)),
+                    StatefulBuilder(
+                        builder: (BuildContext context, StateSetter setState) {
                           return DropdownButton(
                             isExpanded: true,
                             value: dropdownValue,
@@ -113,29 +124,35 @@ class _ClientsPageState extends State<ClientsPage> {
                             }).toList(),
                           );
                         }),
-                      ],
-                    ),
-                  ),
+                  ],
                 ),
-                actions: [
-                  TextButton(
+              ),
+            ),
+            actions: [
+              Consumer<Clients>(
+                builder: (BuildContext context, Clients list, Widget? widget) {
+                  return TextButton(
                       child: const Text("Salvar"),
                       onPressed: () async {
                         setState(() {
-                          clients.add(Client(name: nomeInput.text, email: emailInput.text, type: dropdownValue));
+                          list.clients.add(Client(name: nomeInput.text,
+                              email: emailInput.text,
+                              type: dropdownValue));
                         });
                         Navigator.pop(context);
                       }
-                  ),
-                  TextButton(
-                    child: const Text("Calcelar"),
-                    onPressed: () {
-                      Navigator.pop(context);
-                    }
-                  ),
-                ],
-              );
-            }
+                  );
+                },
+              ),
+              TextButton(
+                  child: const Text("Calcelar"),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  }
+              ),
+            ],
           );
+        }
+    );
   }
 }
