85fdc14982e46f6a0e40013deeb1f3ea978b693c
julia.cavalcante <ccardozojulia@gmail.com>
2023-01-24 14:24:00 -0300

    implementing provider

--- lib/pages/clients_page.dart
+++ lib/pages/clients_page.dart
@@ -14,13 +14,6 @@ class ClientsPage extends StatefulWidget {
 
 class _ClientsPageState extends State<ClientsPage> {
 
-  List<Client> clients = [
-    Client(name: 'Geraldo', email: 'leo@email.com', type: ClientType(name: 'Platinum', icon: Icons.credit_card)),
-    Client(name: 'Paulo', email: 'leo@email.com', type: ClientType(name: 'Golden', icon: Icons.card_membership)),
-    Client(name: 'Caio', email: 'leo@email.com', type: ClientType(name: 'Titanium', icon: Icons.credit_score)),
-    Client(name: 'Ruan', email: 'ruan@email.com', type: ClientType(name: 'Diamond', icon: Icons.diamond)),
-  ];
-
   List<ClientType> types = [
     ClientType(name: 'Platinum', icon: Icons.credit_card),
     ClientType(name: 'Golden', icon: Icons.card_membership),
